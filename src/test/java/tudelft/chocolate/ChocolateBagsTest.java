34d57d87cea3edd36ed76ef79261bc6d599f1c41
wences-sanchez <wenceslaosanchezpino@gmail.com>
2021-04-24 13:04:19 +0200

    Add test cases to new partition
    A new partition 'includeBigAndSmallBars' has been
    created which refer to the case:
    * Big and also small bars are included in the solution

--- src/test/java/tudelft/chocolate/ChocolateBagsTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsTest.java
@@ -21,17 +21,8 @@ public class ChocolateBagsTest {
     }
 
     @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
-    @CsvSource({
-        "0,3,17,-1", "1,3,17,-1", "2,3,17,2", "3,3,17,2",
-        "0,3,12,-1", "1,3,12,-1", "2,3,12,2", "3,3,12,2"})
-    public void bigAndSmallBars(int small, int big, int total, int expectedResult) {
-        int result = new ChocolateBags().calculate(small, big, total);
-        Assertions.assertEquals(expectedResult, result);
-    }
-
-    @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
-    @CsvSource({ "4,2,3,3", "3,2,3,3", "2,2,3,-1", "1,2,3,-1" })
-    public void onlySmallBars(int small, int big, int total, int expectedResult) {
+    @CsvSource({"2,5,17,2", "1,5,17,-1", "3,5,17,2", "2,5,15,0", "5,2,13,3"})
+    public void includeBigAndSmallBars(int small, int big, int total, int expectedResult) {
         int result = new ChocolateBags().calculate(small, big, total);
         Assertions.assertEquals(expectedResult, result);
     }
