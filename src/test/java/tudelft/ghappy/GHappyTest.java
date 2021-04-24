324081ed7b7eb80be40abdecef104753ed11a1ff
wences-sanchez <wenceslaosanchezpino@gmail.com>
2021-04-24 14:51:33 +0200

    Create tests
    The test strategy is to divide the cases in this
    equivalence partitions:
    * The 'g' is preceded (or followed) by another 'g'
    * The 'g' is not with other 'g' at their side

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,22 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    @ParameterizedTest(name = "str={0}")
+    @CsvSource({"gg", "xxgg", "xggxx"})
+    public void gIsPrecededOrFollowedByOtherGs(String str) {
+        Assertions.assertTrue(new GHappy().gHappy(str));
+    }
+
+    @ParameterizedTest(name = "str={0}")
+    @CsvSource({"g", "xg", "gxx", "gxxgx", "xxgxggx"})
+    public void gIsNotPrecededNorFollowedByOtherGs(String str) {
+        Assertions.assertFalse(new GHappy().gHappy(str));
+    }
+
 }
