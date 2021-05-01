e007fa515c765b4fc3aa0d0bb4454763164cb659
wences-sanchez <wenceslaosanchezpino@gmail.com>
2021-05-01 13:28:53 +0200

    Improved original test suite

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -2,19 +2,26 @@ package tudelft.countletters;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CountLettersTest {
 
-    @Test
-    public void multipleMatchingWords() {
-        int words = new CountLetters().count("cats|dogs");
-        Assertions.assertEquals(2, words);
+    @ParameterizedTest(name = "input={0}, expected={1}")
+    @CsvSource({"cats|dogs,2", "bear|dog,1", "bear|fear,2"})
+    public void multipleMatchingWords(String input, int expected) {
+        int words = new CountLetters().count(input);
+        Assertions.assertEquals(expected, words);
     }
 
-    @Test
     public void lastWordDoesNotMatch() {
         int words = new CountLetters().count("cats|dog");
         Assertions.assertEquals(1, words);
+    }
+
+    public void lastWordMatches(){
+        int words = new CountLetters().count("cats|bear");
+        Assertions.assertEquals(2, words);
     }
 
 }
