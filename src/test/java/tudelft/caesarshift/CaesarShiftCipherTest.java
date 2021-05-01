8a696db36474464ac490091ec4949420e51bf42e
wences-sanchez <wenceslaosanchezpino@gmail.com>
2021-05-01 11:56:51 +0200

    Create Test Suite for CaesarShiftCipher class
    The following two partition equivalences
    have been created:
    * The shift is positive. So the input letters should
    increased mod 26
    * The shift is negative. So the input letters should
    decreased mod 26
    
    The boundary 0 is included in both partitions for
    completeness.

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,31 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CaesarShiftCipherTest {
 
+    private CaesarShiftCipher caesarShiftCipher;
+
+    @BeforeEach
+    void setUp() {
+        caesarShiftCipher = new CaesarShiftCipher();
+    }
+
+    @ParameterizedTest(name = "letters={0}, shift={1}, expected={2}")
+    @CsvSource({"abc,1,bcd", "whz,0,whz", "xyz,3,abc"})
+    public void lettersShouldBeIncreased(String s, int shift, String expected) {
+        assertEquals(expected, caesarShiftCipher.CaesarShiftCipher(s, shift));
+    }
+
+    @ParameterizedTest(name = "letters={0}, shift={1}, expected={2}")
+    @CsvSource({"jhz,-1,igy", "azh,0,azh", "axb,-2,yvz"})
+    public void lettersShouldBeDecreased(String s, int shift, String expected) {
+        assertEquals(expected, caesarShiftCipher.CaesarShiftCipher(s, shift));
+    }
 }
